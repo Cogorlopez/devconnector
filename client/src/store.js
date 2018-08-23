b5fd93afd8f97f124d8d7bc20755d611a5fbaf99
Cogorlopez <Cogorlopez@gmail.com>
2018-08-23 07:15:36 -0500

    Fixed bug in store.js. Redux dev tools were breaking IE and Edge functionality

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,21 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/';
 
 const initialState = {};
 
 const middleware = [thunk];
 
+// Allows Redux to connect to chrome dev tools extension.
+// Putting it in a variable fixes IE and Edge issues.
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware)),
+  devTools
 );
 
 export default store;
