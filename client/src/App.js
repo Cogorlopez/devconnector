3631341a7995f5d6ab9643068dedaf2f685b9dcf
Cogorlopez <Cogorlopez@gmail.com>
2018-09-18 16:01:07 -0500

    Started on profile actions and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,10 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -28,6 +30,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // TODO: Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
   }
@@ -44,7 +47,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
+
             <Footer />
           </div>
         </Router>
